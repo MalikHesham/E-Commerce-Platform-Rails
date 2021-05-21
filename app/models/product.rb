e871b6d29269331d875e5a37ed4cb803bcaea5f1
John George <Sci.JohnGeorge.2017@alexu.edu.eg>
2021-05-21 18:53:20 +0200

    Adding UI filters

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,7 +9,8 @@ class Product < ApplicationRecord
   scope :filter_by_brand, -> (brand) { joins(:brand).where(:brands => { :name => brand.downcase }) }
   scope :filter_by_price_lte, -> (price) { where("price <= ?", price) }
   scope :filter_by_price_gte, -> (price) { where("price >= ?", price) }
-  
+  # scope :filter_by_seller, -> (seller) { where(:id => seller.to_i) }
+
   scope :search_by_title_or_description, -> (q) {where("title like ? or description like ?", "%#{q}%", "%#{q}%")}
 
 
