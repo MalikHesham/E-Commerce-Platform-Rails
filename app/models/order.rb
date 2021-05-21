10d700efcf54afcbe3d9800c483faf1d785acfff
Malik Hesham <malikhesham@outlook.com>
2021-05-21 19:50:28 +0200

    adding status to orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
+  enum status: %i[pending confirmed delivered]
   belongs_to :user
   has_many :product_adapter, as: :purchasable
 end
--- db/migrate/20210517193727_create_orders.rb
+++ db/migrate/20210517193727_create_orders.rb
@@ -3,7 +3,6 @@ class CreateOrders < ActiveRecord::Migration[5.2]
     create_table :orders do |t|
       t.references :user, foreign_key: true
       t.float :total
-
       t.timestamps
     end
   end
