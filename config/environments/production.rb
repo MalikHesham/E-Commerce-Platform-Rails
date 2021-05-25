e2d228ec3ddb109e752d23e326576ee410036777
John George <Sci.JohnGeorge.2017@alexu.edu.eg>
2021-05-25 20:42:21 +0200

    add email vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,16 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV.fetch("EMAIL_HOST", 'smtp.gmail.com'),
+    port:                 ENV.fetch("EMAIL_PORT", "587").to_i,
+    domain:               ENV.fetch("DOMAIN", "localhost"),
+    user_name:            ENV.fetch("EMAIL_HOST_USER", '<username>'),
+    password:             ENV.fetch("EMAIL_HOST_PASSWORD", '<password>'),
+    authentication:       'plain',
+    enable_starttls_auto: true }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
